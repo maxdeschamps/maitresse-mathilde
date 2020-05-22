0e8591a499025533ffa75b44e7a67d295d538f96
maxdeschamps <zemoax@hotmail.fr>
2020-05-22 23:50:00 +0200

    ajout menu + debut animations - a terminer

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,3 +1,12 @@
 window.onload = function() {
-  
+  // Animation menu mobile
+  var toggleBtn = document.getElementById('toggle-nav'),
+      mobileMenu = document.getElementById('menu-mobile');
+
+  if (toggleBtn && mobileMenu) {
+    toggleBtn.addEventListener('click', function() {
+      toggleBtn.classList.toggle('active');
+      mobileMenu.classList.toggle('active');
+    });
+  }
 };
