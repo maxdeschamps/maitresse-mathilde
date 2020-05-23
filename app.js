6bb0e9dc3683937413ca2ef0f0732e71a3660337
maxdeschamps <zemoax@hotmail.fr>
2020-05-23 17:53:58 +0200

    creation des pages par niveau + insertion des liens dans les menus

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const twig = require('twig');
 const express = require('express');
 const request = require('request');
 
+const classes = require('./modules/classes');
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -16,17 +18,70 @@ app.get('/', function(req, res) {
 
 // Cours
 app.get('/les-cours', function(req, res) {
-  res.render('pages/cours/index.twig');
+  res.render('pages/cours/index.twig', { niveaux: classes.niveaux });
+});
+
+app.get('/les-cours/:niveau', function(req, res) {
+  var niveauParam = req.params.niveau,
+      niveau = null;
+
+  classes.niveaux.forEach(function(el) {
+    if (el.toLowerCase() == niveauParam.toLowerCase()) {
+      niveau = el;
+    }
+  });
+
+  console.log(niveau)
+
+  if (niveau) {
+    res.render('pages/cours/niveau.twig', { niveaux: classes.niveaux, niveau: niveau });
+  } else {
+    res.render('pages/error.twig', { niveaux: classes.niveaux });
+  }
 });
 
 // Vidéos
 app.get('/les-videos', function(req, res) {
-  res.render('pages/video/index.twig');
+  res.render('pages/video/index.twig', { niveaux: classes.niveaux });
+});
+
+app.get('/les-videos/:niveau', function(req, res) {
+  var niveauParam = req.params.niveau,
+      niveau = null;
+
+  classes.niveaux.forEach(function(el) {
+    if (el.toLowerCase() == niveauParam.toLowerCase()) {
+      niveau = el;
+    }
+  });
+
+  if (niveau) {
+    res.render('pages/video/niveau.twig', { niveaux: classes.niveaux, niveau: niveau });
+  } else {
+    res.render('pages/error.twig', { niveaux: classes.niveaux });
+  }
 });
 
 // Exercices
 app.get('/les-exercices', function(req, res) {
-  res.render('pages/exercice/index.twig');
+  res.render('pages/exercice/index.twig', { niveaux: classes.niveaux });
+});
+
+app.get('/les-exercices/:niveau', function(req, res) {
+  var niveauParam = req.params.niveau,
+      niveau = null;
+
+  classes.niveaux.forEach(function(el) {
+    if (el.toLowerCase() == niveauParam.toLowerCase()) {
+      niveau = el;
+    }
+  });
+
+  if (niveau) {
+    res.render('pages/exercice/niveau.twig', { niveaux: classes.niveaux, niveau: niveau });
+  } else {
+    res.render('pages/error.twig', { niveaux: classes.niveaux });
+  }
 });
 
 // Questions
@@ -36,7 +91,7 @@ app.get('/les-questions', function(req, res) {
 
 // 404
 app.get('*', function(req, res) {
-  res.render('pages/error.twig');
+  res.render('pages/error.twig', { niveaux: classes.niveaux });
 });
 
 app.listen(app.settings.port);
---
+++ modules/classes.js
@@ -0,0 +1,8 @@
+var niveaux = [
+  'CP',
+  'CE1',
+  'CE2',
+  'CM1',
+  'CM2'
+];
+exports.niveaux = niveaux;
