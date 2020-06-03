e2375d1ce75e1a53cfc91eb0bd77144735b74eb3
maxdeschamps <zemoax@hotmail.fr>
2020-06-03 13:59:33 +0200

    animation des sous menu à l ouverture

--- public/assets/script.js
+++ public/assets/script.js
@@ -3,7 +3,9 @@ window.onload = function() {
   var toggleBtn = document.getElementById('toggle-nav'),
       mobileMenu = document.getElementById('menu-mobile'),
       openSubMenuBtn = document.getElementsByClassName('open-sub-menu'),
-      closeSubMenuBtn = document.getElementsByClassName('close-sub-menu');
+      closeSubMenuBtn = document.getElementsByClassName('close-sub-menu'),
+      posLiSubMenu = [],
+      sizesLiSubMenu = [];
 
   // Ouverture et fermeture du menu mobile
   if (toggleBtn && mobileMenu) {
@@ -21,8 +23,12 @@ window.onload = function() {
         }
 
         if (subMenu) {
-          subMenu.parentNode.classList.remove('active');
+          subMenu.children[0].children[2].style.color = 'black';
+          subMenu.children[0].style.width = sizesLiSubMenu[i] + 'px';
+
           subMenu.classList.remove('active');
+          subMenu.classList.remove('animate');
+          subMenu.children[0].classList.remove('animate');
         }
       }
     });
@@ -32,7 +38,6 @@ window.onload = function() {
   if (openSubMenuBtn) {
     for (let i=0; i<openSubMenuBtn.length; i++) {
       openSubMenuBtn[i].addEventListener('click', function(el) {
-
         let subMenu = null;
         for (let j=0; j<el.target.parentNode.children.length; j++) {
           if (el.target.parentNode.children[j].nodeName == 'UL' && el.target.parentNode.children[j].classList.contains('sub-menu')) {
@@ -41,10 +46,49 @@ window.onload = function() {
         }
 
         if (subMenu) {
-          subMenu.parentNode.classList.add('active');
+          // Positionnement du first li
+          subMenu.children[0].style.top = posLiSubMenu[i].top + 7.5 + 'px';
+          subMenu.children[0].style.left = posLiSubMenu[i].left - 4 + 'px';
+          subMenu.children[0].style.transition = 'none';
+
           subMenu.classList.add('active');
+
+          // Animation du first li
+          setTimeout(() => {
+            subMenu.children[0].children[2].style.color = 'white';
+          }, 100)
+
+          setTimeout(() => {
+            subMenu.children[0].style.top = 0 + 'px';
+            subMenu.children[0].style.left = 0 + 'px';
+            subMenu.children[0].style.width = 100 + '%';
+            subMenu.children[0].style.transition = '.8s ease';
+            subMenu.children[0].classList.add('animate');
+          }, 300);
+
+          setTimeout(() => {
+            subMenu.classList.add('animate');
+          }, 1100);
+
+          subMenu.parentNode.classList.add('active');
         }
       });
+
+      // Attribution des tailles des 1er li des sous menu (permettant l'animation)
+      let subMenu = null;
+      for (let j=0; j<openSubMenuBtn[i].parentNode.children.length; j++) {
+        if (openSubMenuBtn[i].parentNode.children[j].nodeName == 'UL' && openSubMenuBtn[i].parentNode.children[j].classList.contains('sub-menu')) {
+          subMenu = openSubMenuBtn[i].parentNode.children[j];
+        }
+      }
+
+      if (subMenu) {
+        sizesLiSubMenu[i] = subMenu.children[0].getBoundingClientRect().width;
+        subMenu.children[0].style.width = sizesLiSubMenu[i] + 'px';
+
+        posLiSubMenu[i] = { top  : subMenu.parentNode.children[0].getBoundingClientRect().top,
+                            left : subMenu.parentNode.children[0].getBoundingClientRect().left }
+      }
     }
   }
 
@@ -53,12 +97,60 @@ window.onload = function() {
     for (let i=0; i<closeSubMenuBtn.length; i++) {
       closeSubMenuBtn[i].addEventListener('click', function(el) {
         if (el.target.parentNode.parentNode.nodeName == 'UL' && el.target.parentNode.parentNode.classList.contains('sub-menu')) {
-          el.target.parentNode.parentNode.parentNode.classList.remove('active');
+          el.target.parentNode.parentNode.children[0].children[2].style.color = 'black';
+
+          el.target.parentNode.parentNode.children[0].style.width = sizesLiSubMenu[i] + 'px';
+          el.target.parentNode.parentNode.children[0].style.top = posLiSubMenu[i].top + 7.5 + 'px';
+          el.target.parentNode.parentNode.children[0].style.left = posLiSubMenu[i].left - 4 + 'px';
+
+          el.target.parentNode.parentNode.classList.remove('animate');
           el.target.parentNode.parentNode.classList.remove('active');
+          el.target.parentNode.parentNode.children[0].classList.remove('animate');
+        }
+
+        let subMenu = null;
+        for (let j=0; j<openSubMenuBtn[i].parentNode.children.length; j++) {
+          if (openSubMenuBtn[i].parentNode.children[j].nodeName == 'UL' && openSubMenuBtn[i].parentNode.children[j].classList.contains('sub-menu')) {
+            subMenu = openSubMenuBtn[i].parentNode.children[j];
+          }
+        }
+
+        if (subMenu) {
+          subMenu.children[0].style.width = sizesLiSubMenu[i] + 'px';
         }
       });
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // FOOTER
   var footerEl = document.getElementById('footer'),
